42be8cdbc081004563227d9f80e12d4d08be2a13
Saurabh-997 <40665904+Saurabh-997@users.noreply.github.com>
2020-04-22 13:17:21 -0400

    Add files via upload

---
+++ JavaBankApplication/src/Account.java
@@ -0,0 +1,34 @@
+
+public class Account {
+	// Fields
+
+		private String accountNumber;
+		private double balance = 0;
+		// Constructor
+
+		public Account(String accountNumber) {
+			this.accountNumber = accountNumber;
+		}
+		// Methods
+		public void deposit(double amount) {
+			this.balance += amount;
+		}
+		public String getAccountNumber() {
+			return accountNumber;
+		}
+
+		public double getBalance() {
+			return balance;
+		}
+		
+		public boolean withdraw(double amount) {
+			if (balance <= amount) {
+				return false;
+			} else {
+				balance = balance - amount;
+				return true;
+			}
+		}
+			
+
+}
